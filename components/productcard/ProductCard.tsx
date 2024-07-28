23597073f7c3f4307648ca437d817c09439bddc0
WIsavachart <wisavachart.s@gmail.com>
2024-07-28 11:37:24 +0700

    Edit card & card warp & text Wrap

--- components/productcard/ProductCard.tsx
+++ components/productcard/ProductCard.tsx
@@ -17,9 +17,10 @@ const ProductCard = ({ productData }: ProductCardPROP) => {
   return (
     <>
       <div
-        className="drop-shadow-xl rounded-[14px] cursor-pointer"
+        className="drop-shadow-xl rounded-[14px] cursor-pointer bg-white "
         onClick={handleClick}>
-        <div className="relative flex-1">
+        {/* TOP */}
+        <div className="relative ">
           <div
             className={
               productData.isNewArrival
@@ -39,7 +40,7 @@ const ProductCard = ({ productData }: ProductCardPROP) => {
                 : null}
             </span>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col ">
             <div className="overflow-hidden  rounded-t-[14px] ">
               <Image
                 src={
@@ -50,28 +51,32 @@ const ProductCard = ({ productData }: ProductCardPROP) => {
                 alt="img"
               />
             </div>
-            <div className="stage1:text-[13px] stage2:text-[16px] flex flex-col px-4 py-4 gap-2 bg-white rounded-b-[14px] ">
-              {productData?.category?.title ? (
-                <h6>{productData.category.title}</h6>
-              ) : (
-                <h6>no category</h6>
-              )}
-              <h2 className="prompt-semibold stage1:text-[14px] stage2:text-lg">
-                {productData.title}
-              </h2>
-              <div className="flex justify-between">
-                <h1 className="stage1:text-[16px] stage2:text-xl prompt-extrabold text-xl">
-                  {productData.price} บาท
-                </h1>
-                <button
-                  className="px-2 py-1 bg-bbred text-white rounded-[5px]"
-                  onClick={handleClick}>
-                  <span className="stage1:text-[13px] stage2:text-[16px] ">
-                    ดูสินค้า
-                  </span>
-                </button>
-              </div>
-            </div>
+          </div>
+        </div>
+        {/* Detail */}
+        <div className="stage1:text-[13px] h-[130px] stage2:h-[160px] stage2:text-[16px] flex flex-col justify-between px-4 py-4 gap-2 bg-white rounded-b-[14px]">
+          <div>
+            {" "}
+            {productData?.category?.title ? (
+              <h6>{productData.category.title}</h6>
+            ) : (
+              <h6>no category</h6>
+            )}
+            <h2 className="prompt-semibold stage1:text-[14px] stage2:text-lg card_preview_title">
+              {productData.title}
+            </h2>
+          </div>
+          <div className=" flex justify-between">
+            <h1 className="stage1:text-[16px] stage2:text-xl prompt-extrabold text-xl">
+              {productData.price} บาท
+            </h1>
+            <button
+              className="px-2 py-1 bg-bbred text-white rounded-[5px]"
+              onClick={handleClick}>
+              <span className="stage1:text-[13px] stage2:text-[16px] ">
+                ดูสินค้า
+              </span>
+            </button>
           </div>
         </div>
       </div>
