b2dcb6b33630d7eaa30eb763abe7abe76341dc6a
WIsavachart <wisavachart.s@gmail.com>
2024-08-09 08:33:33 +0700

    Add Gap

--- components/home/SectionBestSeller.tsx
+++ components/home/SectionBestSeller.tsx
@@ -31,7 +31,7 @@ const SectionBestSeller = () => {
       </div>
 
       {/* SECTION WRAPPER */}
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 lg:gap-8 md:gap-8 sm:gap-8 gap-2 lg:px-0 md:px-6 sm:px-6 px-6">
+      <div className="grid gap-6 grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 lg:gap-8 md:gap-8 sm:gap-8  lg:px-0 md:px-6 sm:px-6 px-6">
         {randomBestSeller.map((data) => (
           <ProductCard key={data._id} productData={data} />
         ))}
